52343d7cd8ab7904f06ee540956ab4b28044bbb2
Antonio Montero <019101861@uandina.edu.pe>
2022-05-04 23:30:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/motionapp/motion2.java
@@ -0,0 +1,14 @@
+package com.example.motionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class motion2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion2);
+    }
+}
